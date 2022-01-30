3aa04781de6f495255991bcd9bdd0ae6ec28c4ba
nawodyaishan <nawodyain@gmail.com>
2022-01-30 15:54:13 +0530

    Fix

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ print(">>>>>>>>>>>>>>>>>>>>>>>>>")
 print("Welcome to Tip Calculator!")
 print("__________________________")
 
-total = float(input("What was the total bill? "))
+total = float(input("What was the total bill? $"))
 perc = float(input("What percentage tip would you like to give ? "))
 no = float(input("How many people to split the bill? "))
 
-new_total = total * (perc + 1)
+new_total = total * ((perc/100) + 1)
 
 final = (round(new_total,2)) / no
 
-print(f"Each person should pay: {final}")
+print(f"Each person should pay: ${final}")
 
 
 
