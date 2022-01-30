e36dba7e754585fe5863424bb7cd7843220b27bd
nawodyaishan <nawodyain@gmail.com>
2022-01-30 15:49:00 +0530

    Update

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print(">>>>>>>>>>>>>>>>>>>>>>>>>")
 print("Welcome to Tip Calculator!")
 print("__________________________")
 
+total = float(input("What was the total bill? "))
+perc = float(input("What percentage tip would you like to give ? "))
+
+
+
