c2d5ec37b899965d53ffd1b12ec6ee10409014a0
nawodyaishan <nawodyain@gmail.com>
2022-02-06 20:57:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ print(">>>>>>>>>>>>>>>>>>>>>>>>>")
 print("Welcome to Tip Calculator!")
 print("__________________________")
 
+# Taking Inputs
+
 total = float(input("What was the total bill? $"))
+
 perc = float(input("What percentage tip would you like to give ? "))
+
 no = float(input("How many people to split the bill? "))
 
 new_total = total * ((perc/100) + 1)
